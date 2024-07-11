a3899c63aed74c7bc4a7d1cb8f8e49750f052789
Diogo Gonçalves <a101919@alunos.uminho.pt>
2024-07-11 17:39:22 +0100

    bug??

--- backend/src/app.js
+++ backend/src/app.js
@@ -135,7 +135,7 @@ app.use("/api/inventory", inventoryRoutes);
 app.use("/api/stats", statsRoutes);
 
 // Porta do servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
   console.log(`Swagger disponível em: http://localhost:${PORT}/api-docs`);
