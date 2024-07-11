b2e7847c7d384aa589c99ca2f259f60b43b75e1a
Diogo Gonçalves <a101919@alunos.uminho.pt>
2024-07-11 16:44:04 +0100

    add "0.0.0.0"

--- backend/src/app.js
+++ backend/src/app.js
@@ -94,7 +94,7 @@ app.use("/api/stats", statsRoutes);
 
 // Porta do servidor
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
   console.log(`Swagger disponível em: http://localhost:${PORT}/api-docs`);
 });
