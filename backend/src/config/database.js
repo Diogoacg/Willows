6333c086bbd26e7ac9c85e07141544365a70b92b
Diogo Gonçalves <a101919@alunos.uminho.pt>
2024-07-11 15:57:56 +0100

    teste

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,5 @@
-const { Sequelize } = require("sequelize");
 require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,7 +8,20 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      connectTimeout: 60000,
+    },
   }
 );
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 module.exports = sequelize;
